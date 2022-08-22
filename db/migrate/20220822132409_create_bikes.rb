66a935359952a90904816859a9c07ddca6655077
dk <denis.kolokolov@icloud.com>
2022-08-22 15:40:32 +0200

    setup of all models

---
+++ db/migrate/20220822132409_create_bikes.rb
@@ -0,0 +1,11 @@
+class CreateBikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bikes do |t|
+      t.string :title
+      t.integer :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
