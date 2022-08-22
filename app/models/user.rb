b62abf73b68e1233e99338e1726bd188bf77ce89
dk <denis.kolokolov@icloud.com>
2022-08-22 17:53:44 +0200

    seeds upgrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bikes, through: :bookings
+  has_many :bikes, dependent: :destroy
   has_many :bookings
 end
