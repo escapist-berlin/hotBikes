df23e36bb9e2461a6d3544c65d63561441c5f787
dk <denis.kolokolov@icloud.com>
2022-08-22 17:09:54 +0200

    seeding bikes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bikes, through: :bookings
+  has_many :bookings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 20 fake bikes...'
+20.times do
+  bike = Bike.new(
+    title: Faker::Company.name,
+    price: rand(10..20),
+    user_id: 1
+  )
+  bike.save!
+end
+puts 'Finished!'
