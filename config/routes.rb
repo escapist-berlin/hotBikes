d4423363da9df2ae60de1aebac4162a48b80c4b1
dk <denis.kolokolov@icloud.com>
2022-08-23 15:10:36 +0200

    Show, new, create finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "bikes", to: "bikes#index"
-  resources :bikes, only: [:index, :new]
+  resources :bikes, only: [:index, :new, :create, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: :destroy
   # Defines the root path route ("/")
   # root "articles#index"
 end
